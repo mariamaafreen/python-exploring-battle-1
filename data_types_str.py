8cb20f448e1d4f46d7504ed5bf5cddb9d0c4f136
Mariam <sanubia99@gmail.com>
2019-06-24 23:46:42 +0530

    data types - strings

---
+++ data_types_str.py
@@ -0,0 +1,26 @@
+string1 = "this is "
+string2 = "awesome"
+
+print(type(string1))
+
+string = string1 + string2
+
+print(string)
+
+print(type(string))
+
+print(len(string1))
+print(len(string))
+
+print(string.index("a"))
+
+print(string.count("s"))
+
+print(string[3:10:2])
+
+print(string.upper())
+print(string.lower())
+
+print(string.startswith("is"))
+print(string.endswith("awesome"))
+
