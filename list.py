25b42eef3a52ec8b308e09f5875b190569e54a19
Mariam <sanubia99@gmail.com>
2019-06-25 01:00:13 +0530

    list

---
+++ list.py
@@ -0,0 +1,41 @@
+# creating list
+
+l1 = ['hello']
+l2 = [['1','4'],['hi']]
+l3 = []
+
+
+print(l1)
+print(l2)
+
+
+#addition of elements
+
+l3.append(1)
+l3.append(2)
+l3.append(3)
+
+print(l3)
+
+for i in range(4,8):
+    l3.append(i)
+
+print(l3)
+
+l3.extend([9,10,11])
+print(l3)
+
+l1.insert(0,'hi')
+print(l1)
+
+#accessing elements in a list
+
+print(l3[0])
+print(l2[0] [1])
+
+print(l3 [-1])
+
+
+
+
+
