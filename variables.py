9e2867fb8c4a6a5da4cb84d17d9a75f9ad0a6d86
Mariam <sanubia99@gmail.com>
2019-06-25 00:06:07 +0530

    data types - strings

---
+++ variables.py
@@ -0,0 +1,24 @@
+# variable declaration
+
+var1 = 100
+var2 = 200
+var3 = '1'
+var4 = '2'
+
+print(var1)
+
+#cancatenate variables
+
+var = var1 + var2 
+variable = var1 + int(var3)
+
+print(var)
+
+#delete a variable
+
+del(var2)
+
+#variable conversion
+
+print(float(var1))
+print(int(var3))
