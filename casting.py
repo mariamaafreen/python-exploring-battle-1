97f167c503c1ed0ed9eb53ee8f8094c12abe3891
Mariam <sanubia99@gmail.com>
2019-06-25 20:18:46 +0530

    casting

---
+++ casting.py
@@ -0,0 +1,23 @@
+#integers
+
+x = int(1)   
+y = int(2.8)
+print(x)
+print(y)
+
+#float
+
+z = float("3")   
+w = float("4.2")
+print(z)
+print(w)
+
+
+#string
+
+a = str("s1")
+b = str(2)
+c = str(3.0)
+print(a)
+print(b)
+print(c)
