7821b559b76d0ccf9fdabc82bef369102937cc0f
Mariam <sanubia99@gmail.com>
2019-06-25 19:58:19 +0530

    tuples

---
+++ tuples.py
@@ -0,0 +1,29 @@
+#create a tuple
+
+tup = ("apple", "banana", "cherry")
+print(tup)
+
+#acessing tuple items
+
+print(tup[1])
+
+#checking if an item exist in a tuple
+
+if "apple" in tup:
+  print("Yes, 'apple' is in the fruits tuple")
+
+else:
+  print("it does not exist")
+
+#length of a tuple
+
+print(len(tup))
+
+#count
+
+print(tup.count("apple"))
+
+#index
+
+print(tup.index("cherry"))
+
