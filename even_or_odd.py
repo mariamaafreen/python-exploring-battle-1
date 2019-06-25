03081c7e4f98c6e396104095ae8bd7c3f04dff9e
Mariam <sanubia99@gmail.com>
2019-06-25 19:52:40 +0530

    loops

---
+++ even_or_odd.py
@@ -0,0 +1,14 @@
+#using for loop
+
+for i in range(1,25):
+    if i % 2 == 0:
+        print(i)
+
+
+#using while loop
+
+i = 1
+while(i<=10):
+    if i % 2 ==1:
+        print(i)
+    i = i + 1            
